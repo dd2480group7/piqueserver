bc40d6fbc9d072c8cdb3f4adec0af3f01be9d317
Jacobs Apple ID <sorme@kth.se>
2019-02-14 13:58:50 +0100

    Fix errors in test_blockinfo.py #3

--- tests/piqueserver/test_blockinfo.py
+++ tests/piqueserver/test_blockinfo.py
@@ -13,8 +13,7 @@ class TestBlockinfo(unittest.TestCase):
 
         self.assertRaises(ValueError,blockinfo.grief_check,connection,"#123",minutes=-10) # Minutes less than 0
 
-class TestBlockinfo(unittest.TestCase):
-    def test_grief_check1(self):
+    def test_grief_check2(self):
         connection = Mock()
         connection.colors = True
         connection.protocol = Mock()
