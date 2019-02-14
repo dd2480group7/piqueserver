7af95d5302dfa444bccf7b14c49ef2092ce81137
Jacobs Apple ID <sorme@kth.se>
2019-02-14 13:37:50 +0100

    Work on testing blockinfo.py #3

--- tests/piqueserver/test_blockinfo.py
+++ tests/piqueserver/test_blockinfo.py
@@ -1,5 +1,6 @@
 import unittest
 import piqueserver.scripts.blockinfo as blockinfo
+from piqueserver.commands import CommandError
 from unittest.mock import Mock
 
 
@@ -10,4 +11,13 @@ class TestBlockinfo(unittest.TestCase):
         connection.protocol = Mock()
         connection.protocol.players = {123:"Jerry"}
 
-        self.assertRaises(ValueError,blockinfo.grief_check,connection,"#123",minutes=-10)
+        self.assertRaises(ValueError,blockinfo.grief_check,connection,"#123",minutes=-10) # Minutes less than 0
+
+class TestBlockinfo(unittest.TestCase):
+    def test_grief_check1(self):
+        connection = Mock()
+        connection.colors = True
+        connection.protocol = Mock()
+        connection.protocol.players = {124:"Jerry"} # Wrong id/name for Jerry here
+
+        self.assertRaises(CommandError,blockinfo.grief_check,connection,"#123",minutes=123)
