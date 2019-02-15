915d8fd83f7110798ce3ae3db07ff4dd6ee1d29b
Daniel <b.daniel.karlsson@gmail.com>
2019-02-15 14:49:31 +0100

    Added test for on_input_data_received #6 #3
    three simple tests for the function
    tests returns and some paths

--- tests/pyspades/test_player.py
+++ tests/pyspades/test_player.py
@@ -27,3 +27,36 @@ class BaseConnectionTest(unittest.TestCase):
             ply.on_new_player_recieved(ex_ply)
 
             self.assertEqual(ply.team, team)
+
+    #testing first return None
+    def test_on_input_data_recieved1(self):
+        ply = player.ServerConnection(Mock(), Mock())
+        input_ply = contained.InputData()
+        self.assertEqual(ply.on_input_data_recieved(input_ply), None)
+
+    #testing change of jump due to velocity
+    def test_on_input_data_recieved2(self):
+        ply = player.ServerConnection(Mock(), Mock())
+        ply.world_object = Mock()
+        ply.world_object.velocity = Mock()
+        ply.world_object.velocity.z = 1
+        ply.set_hp(10)
+        ply.player_id = 1
+        input_ply = contained.InputData()
+        input_ply.jump = True
+
+        self.assertTrue(input_ply.jump)
+        ply.on_input_data_recieved(input_ply)
+        self.assertFalse(input_ply.jump) #should have changed to false due to velocity
+        ply.on_input_data_recieved(input_ply)
+
+    #testing last return None
+    def test_on_input_data_recieved3(self):
+        ply = player.ServerConnection(Mock(), Mock())
+        ply.filter_visibility_data = True
+        ply.world_object = Mock()
+        ply.freeze_animation = True
+        ply.set_hp(10)
+        ply.player_id = 1
+        input_ply = contained.InputData()
+        self.assertEqual(ply.on_input_data_recieved(input_ply), None)
