0e5da93857d8431cc124b0cb18ed5075600779b6
Jacobs Apple ID <sorme@kth.se>
2019-02-15 10:18:34 +0100

    Add test for score_grief in badmin.py #3

--- tests/piqueserver/test_badmin.py
+++ tests/piqueserver/test_badmin.py
@@ -24,3 +24,21 @@ class TestBadmin(unittest.TestCase):
         player.team_id = 123
 
         self.assertEqual(0,badmin.score_grief(connection,player))
+
+    def test_score_grief3(self):
+        connection = Mock()
+        connection.colors = True
+        connection.protocol.players = []
+
+        player = Mock()
+        player.blocks_removed = [[110,None]]
+        player.name = "Jerry"
+        player.team.id = 123
+        badmin.reactor = Mock()
+
+        def returnZero():
+            return 0
+
+        badmin.reactor.seconds = returnZero
+
+        self.assertEqual(0,badmin.score_grief(connection,player,time=1))
